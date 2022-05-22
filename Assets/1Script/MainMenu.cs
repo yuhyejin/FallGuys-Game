b80ac8fb7724537e6ea93d2b640b24e8968b159c
yuhyejin <74812194+yuhyejin@users.noreply.github.com>
2022-05-22 20:05:44 +0900

    메인 UI 배치
    - 메인 화면 start, Quit 버튼 생성

---
+++ Assets/1Script/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        Debug.Log("시작");
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR    // 유니티 내에서 게임을 멈추게 함.
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
